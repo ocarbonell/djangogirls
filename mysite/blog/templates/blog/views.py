d1f1ae97ab8ea79915ea672056450bfafabf97d7
Oriol Carbonell González <ocarbonell@apsl.net>
2019-09-05 10:12:14 +0200

    ok

---
+++ mysite/blog/templates/blog/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, get_object_or_404
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request,'blog/post_detail.html', {'post':post})
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone
-from django.views.generic import FormView
+from mysite.blog.models import Post
 
-from .models import Post
 
-# Create your views here.
 def post_list(request):
-
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
