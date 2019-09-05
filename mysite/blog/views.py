780bffd42614fbf97f1c5899848d1938885e7ab8
Oriol Carbonell González <ocarbonell@apsl.net>
2019-09-05 11:34:44 +0200

    En aquest punt funciona tot.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
 from django.utils import timezone
-from mysite.blog.models import Post
+from .models import Post
 from django.shortcuts import render, get_object_or_404
 
 
@@ -8,6 +7,7 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request,'blog/post_detail.html', {'post':post})
+    return render(request, 'blog/post_detail.html', {'post': post})
