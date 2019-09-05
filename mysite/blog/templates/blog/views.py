51cecad935665b2e29cb50e685328c1860008d97
Oriol Carbonell González <ocarbonell@apsl.net>
2019-09-05 10:33:22 +0200

    ok

--- mysite/blog/templates/blog/views.py
+++ mysite/blog/templates/blog/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
-from mysite.blog.models import Post
-
 
 def post_detail(request, pk):
+    from mysite.blog.models import Post
     post = get_object_or_404(Post, pk=pk)
     return render(request,'blog/post_detail.html', {'post':post})
